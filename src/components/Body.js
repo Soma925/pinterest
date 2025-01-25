57c42421c18af5cd6619f822b18293ad9f381880
Soma Bar <71581968+Soma925@users.noreply.github.com>
2025-01-26 01:51:31 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -31,12 +31,62 @@ import chiken4 from '../../src/images/chiken4.jpg';
 import multipleImage from '../../src/images/multipleImage.jpg';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPinterest } from '@fortawesome/free-brands-svg-icons';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 export default function Body() {
   return (
     <div>
+    {/* navigation bar */}
+    <nav className="navbar navbar-expand-lg fixed-top">
+        <div className="container-fluid con">
+            <a className="navbar-brand logo" href='/'> <FontAwesomeIcon className='logoicon' icon={faPinterest} />Pinterest</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarScroll">
+                <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" >
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Watch</a>
+                    </li> 
+                    <li className="nav-item">
+                    <a className="nav-link" href="/">Explore</a>
+                    </li>    
+                </ul>
+                <ul class="navbar-nav ">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Business</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Press</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link login" href="/">Log in</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link signup" href="/"  data-bs-toggle="modal" data-bs-target="#exampleModal">Sign up</a>
+                    </li>		
+                </ul>
+            </div>
+        </div>
+    </nav>
+    {/* <!-- Modal --> */}
+    <div className="modal fade" id="exampleModal" data-backdrop="static">
+        <div className="modal-dialog">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div className="modal-body">
+                    <Signup/>
+                </div>
+            </div>
+        </div>
+    </div>
     {/* first part */}
     <div id="demo" className="carousel slide" data-bs-ride="carousel">
         <div className="carousel-indicators">
@@ -175,7 +225,10 @@ export default function Body() {
         </div>   
     </div>
     <div className="footer">
-
+        <a className="footerList" href="/">Terms of service</a>
+        <a className="footerList" href="/">Privacy Policy</a>
+        <a className="footerList" href="/">Help</a>
+        <a className="footerList" href="/">Users</a>
     </div>
     </div>
   )
