b977817f7355d40bcebaa757ec703d0871987dab
Soma Bar <71581968+Soma925@users.noreply.github.com>
2025-01-25 19:14:08 +0530

    Update Pin.js

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -1,29 +1,3 @@
-// import React from 'react'
-// import chiken1 from '../../src/images/chiken1.jpg';
-// import chiken2 from '../../src/images/chiken2.jpg';
-// import chiken3 from '../../src/images/chiken3.jpg';
-// import chiken4 from '../../src/images/chiken4.jpg';
-// export default function pin(props) {
-//   return (
-//     <div style={{
-//       ...styles.pin,
-//       ...styles[props.size]
-//     }}>
-
-//       {/* <img src={chiken1} alt=""/> */}
-      
-//     </div>
-//   )
-// }
-
-// const styles={
-//   pin:{margin:'15px 10px',padding:0,borderRadius:'16px',backgroundColor:'red',position:'relative',overflow:'hidden'},
-//   small:{gridRowEnd:'span 26',overflow:'hidden',position:'relative'},
-//   medium:{gridRowEnd:'span 33',overflow:'hidden',position:'relative'},
-//   large:{gridRowEnd:'span 38',overflow:'hidden',position:'relative'},
-// img:{position:'absolute',width:'100%',height:'100%',ovjectFit:'fill'}
-// }
-
 import React, { useState } from 'react';
 import '../../src/App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
