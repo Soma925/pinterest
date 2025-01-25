bc2a77fa0001e79608d4699a1e8d02d52de971e4
Soma Bar <71581968+Soma925@users.noreply.github.com>
2025-01-26 02:08:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import{Route,Routes}from "react-router-dom";
 import './App.css';
 import Body from './components/Body';
-import Header from './components/Header';
-
+import Finalboards from './components/Finalboard';
 function App() {
   return (
     <>
-    <Header/>
-    <Body/>
-   </>
+  
+
+    <Routes>
+      <Route path="/" element={<Body/>}/>
+      <Route path="/mainpage" element={<Finalboards/>}/>
+    </Routes>
+
+
+    </>
   );
 }
-
 export default App;
